74d1dc29f2d1fa4c2203819d449df875c669750f
Minat24 <minhnhat241203@gmail.com>
2025-09-30 13:32:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
  lconst CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'nguyễn lê uyên nhung',
+    titleWeb: "anh yêu em",
+    introTitle: 'Nguyễn Thanh ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -10,11 +10,11 @@
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/mayanhtrickerthaem304' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Thứ 7, anh qua đón em đi chơi nha.',
+    btnAccept: 'Dạaaa anh <3',
+    messLink: 'https://www.facebook.com/messages/e2ee/t/24136745026014560' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
