164dfe7c0eeb1a34b8300f19c79343203b76975d
Minat24 <minhnhat241203@gmail.com>
2025-09-30 14:26:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
     titleWeb: "anh yêu em",
     introTitle: 'Nguyễn Thanh Thúy',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
@@ -11,7 +11,7 @@
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho em <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Thứ 7, anh qua đón em đi chơi nha.',
